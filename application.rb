83d2bd7e01611a8b688e0510ea733028e806352f
Killy <shengxiang205@foxmail.com>
2013-07-04 17:25:22 +0800

    Fiber化

--- application.rb
+++ application.rb
@@ -5,36 +5,120 @@ require 'base64'
 require 'zlib'
 require 'stringio'
 
+# Copyright (C) 2013 10gen Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'socket'
+require 'timeout'
+
+module Mongo
+  # Wrapper class for Socket
+  #
+  # Emulates TCPSocket with operation and connection timeout
+  # sans Timeout::timeout
+  #
+  class TCPSocket
+
+    def read(maxlen, buffer)
+      # Block on data to read for @op_timeout seconds
+      current_time = Time.now
+      log_info 'start query'
+      if EM.reactor_running?
+        loop do
+          begin
+
+            f     = Fiber.current
+            ready = IO.select([@socket], nil, [@socket], 0.0001)
+
+
+            unless ready
+              EM.add_timer(0.001) do
+                log_info "resume: #{f.object_id}"
+                f.resume
+              end
+
+              Fiber.yield
+            else
+              break;
+            end
+          rescue IOError
+            raise ConnectionFailure
+          end
+        end
+      else
+        begin
+          ready = IO.select([@socket], nil, [@socket], @op_timeout)
+          unless ready
+            raise OperationTimeout œ
+          end
+        rescue IOError
+          raise ConnectionFailure
+        end
+      end
+      log_info 'end'
+      #begin
+      #  ready = IO.select([@socket], nil, [@socket], @op_timeout)
+      #  unless ready
+      #    raise OperationTimeout
+      #  end
+      #rescue IOError
+      #  raise ConnectionFailure
+      #end
+
+      # Read data from socket
+      begin
+        @socket.sysread(maxlen, buffer)
+      rescue SystemCallError, IOError => ex
+        raise ConnectionFailure, ex
+      end
+    end
+  end
+end
+
+
 Dir.mkdir('log') unless File.exist?('log')
-class ::Logger; alias_method :write, :<<; end
+class ::Logger;
+  alias_method :write, :<<;
+end
 case ENV["RACK_ENV"]
-when "production"
-  logger = ::Logger.new("log/production.log")
-  logger.level = ::Logger::WARN
-when "development"
-  logger = ::Logger.new(STDOUT)
-  logger.level = ::Logger::DEBUG
-else
-  logger = ::Logger.new("/dev/null")
+  when "production"
+    logger       = ::Logger.new("log/production.log")
+    logger.level = ::Logger::WARN
+  when "development"
+    logger       = ::Logger.new(STDOUT)
+    logger.level = ::Logger::DEBUG
+  else
+    logger = ::Logger.new("/dev/null")
 end
 
 Cache = Dalli::Client.new(['192.168.10.249:11211'], threadsafe: true, failover: true, namespace: 'data_ocean')
 Sinatra::Synchrony.overload_tcpsocket!
 
 Connection = Mongo::MongoReplicaSetClient.new(
-  [
-    '192.168.10.248:40000',
-    '192.168.10.239:40000',
-    '192.168.10.246:40000'
-  ], :pool_size => 50 )
-DB = Connection.db('data_ocean_production_test_env')
+    [
+        '192.168.10.248:40000',
+        '192.168.10.239:40000',
+        '192.168.10.246:40000'
+    ], :pool_size => 50)
+DB         = Connection.db('data_ocean_production_test_env')
 
 
 class QueryApplication < Sinatra::Base
   module Util
     def self.unpack_raw_args(raw_args)
       zipped_content = Base64.decode64(raw_args)
-      zipped_io = StringIO.new(zipped_content)
+      zipped_io      = StringIO.new(zipped_content)
       JSON.parse(Zlib::GzipReader.new(zipped_io).read, :symbolize_names => true)[:args]
     rescue => e
       []
@@ -45,15 +129,15 @@ class QueryApplication < Sinatra::Base
 
   configure do
     # = Configuration =
-    set :run,             false
+    set :run, false
     set :show_exceptions, development?
-    set :raise_errors,    development?
-    set :logging,         true
-    set :static,          false # your upstream server should deal with those (nginx, Apache)
+    set :raise_errors, development?
+    set :logging, true
+    set :static, false # your upstream server should deal with those (nginx, Apache)
   end
 
   configure :production do
-  end 
+  end
 
   get '/api/v1/query.*' do
     # logger.info "开始访问: #{Time.now.to_f}"
@@ -75,15 +159,15 @@ class QueryApplication < Sinatra::Base
     end
 
     ids = [
-      "connected_droplet_object_ids_#{data_lake_stub_name}",
-      "self_droplet_object_ids_#{data_lake_stub_name}"
+        "connected_droplet_object_ids_#{data_lake_stub_name}",
+        "self_droplet_object_ids_#{data_lake_stub_name}"
     ].inject([]) do |ids, cache_key|
       ids += Cache.get(cache_key)
     end.map { |d| BSON::ObjectId(d.to_s) }
 
     collection = DB['cache_processor_droplets']
     # _id: { '$in' => ids }
-    collection.find( _id: { '$in' => ids }, as: args.first).to_a.to_json
+    collection.find(_id: { '$in' => ids }, as: args.first).to_a.to_json
   end
 end
 
